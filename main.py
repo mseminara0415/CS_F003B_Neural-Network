72b9e5f16bae468c0c49a00aa769fa60070a833e
Matt <matthewseminara@gmail.com>
2021-06-01 21:34:28 -0700

    Updated for assignment 4 LayerList

--- main.py
+++ main.py
@@ -787,6 +787,7 @@ class DoublyLinkedList:
             else:
                 return self._curr.data
 
+
 class LayerList(DoublyLinkedList):
     def __init__(self, inputs: int, outputs: int):
         self.inputs = inputs
