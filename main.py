6adc7d1557404851d18e375a97f4f0ae14cc1b04
mseminara0415 <matthewseminara@gmail.com>
2021-06-07 17:56:34 -0700

    Included add_hidden_layer method to FFBPNetwork. Begin building train method.

--- main.py
+++ main.py
@@ -115,8 +115,8 @@ class NNData:
 
 class LayerType(Enum):
     INPUT = 0
-    HIDDEN = 1
-    OUTPUT = 2
+    OUTPUT = 1
+    HIDDEN = 2
 
 
 class MultiLinkNode(ABC):
@@ -386,6 +386,8 @@ class DoublyLinkedList:
         return self._current.data
 
 
+
+
 class LayerList(DoublyLinkedList):
 
     def __init__(self, inputs, outputs):
@@ -456,13 +458,52 @@ class FFBPNetwork:
         self.outputs = num_outputs
 
     def add_hidden_layer(self, num_nodes: int, position=0):
-        pass
-    # use add layer from LayerList
-    # reset to head
+        """
+        Add hidden layer of Neurodes at specified position.
+        :param num_nodes:
+        :param position:
+        :return:
+        """
+
+        # Default to position right after head node
+        if position == 0:
+            self.network.reset_to_head()
+            self.network.add_layer(num_nodes=num_nodes)
+
+        # Add node in after specified position
+        if position > 0:
+            # Reset to head node
+            self.network.reset_to_head()
+
+            # Move forward i positions
+            for i in range(position-1):
+                self.network.move_forward()
+
+            # Add hidden layer
+            self.network.add_layer(num_nodes=num_nodes)
+
+            # Reset to head
+            self.network.reset_to_head()
 
     def train(self, data_set: NNData, epochs=1000, verbosity=2, order=NNData.Order.RANDOM):
+
         if data_set.pool_is_empty(data_set):
-            raise
+            raise self.EmptySetException
+        else:
+            for epoch in range(epochs):
+                data_set.prime_data(data_set.Set.TRAIN, order=order)
+                while not data_set.pool_is_empty(data_set):
+                    feature_label_pair = data_set.get_one_item(data_set.Set.TRAIN)
+                    feature = feature_label_pair[0]
+                    label = feature_label_pair[1]
+                    inputs = self.network.input_nodes
+                    for node in inputs:
+                        node.set_expected(feature)
+
+
+
+
+
 
         # Get one item
         # Feed it to the input layer
@@ -478,6 +519,108 @@ class FFBPNetwork:
     def test(self, data_set:NNData,order=NNData.Order.SEQUENTIAL):
         pass
 
+def unit_test():
+    errors = False
+    try:
+        # Create a valid small and large dataset to be used later
+        x = [[i] for i in range(10)]
+        y = x
+        our_data_0 = NNData(x, y)
+        x = [[i] for i in range(100)]
+        y = x
+        our_big_data = NNData(x, y, .5)
+
+        # Try loading lists of different sizes
+        y = [[1]]
+        try:
+            our_bad_data = NNData()
+            our_bad_data.load_data(x, y)
+            raise Exception
+        except DataMismatchError:
+            pass
+        except:
+            raise Exception
+
+        # Create a dataset that can be used to make sure the
+        # features and labels are not confused
+        x = [[1.0], [2.0], [3.0], [4.0]]
+        y = [[.1], [.2], [.3], [.4]]
+        our_data_1 = NNData(x, y, .5)
+
+    except:
+        print("There are errors that likely come from __init__ or a "
+              "method called by __init__")
+        errors = True
+
+    # Test split_set to make sure the correct number of examples are in
+    # each set, and that the indices do not overlap.
+    try:
+        our_data_0.split_set(.3)
+        print(f"Train Indices:{our_data_0._train_indices}")
+        print(f"Test Indices:{our_data_0._test_indices}")
+
+        assert len(our_data_0._train_indices) == 3
+        assert len(our_data_0._test_indices) == 7
+        assert (list(set(our_data_0._train_indices +
+                         our_data_0._test_indices))) == list(range(10))
+    except:
+        print("There are errors that likely come from split_set")
+        errors = True
+
+    # Make sure prime_data sets up the deques correctly, whether
+    # sequential or random.
+    try:
+        our_data_0.prime_data(order=NNData.Order.SEQUENTIAL)
+        assert len(our_data_0._train_pool) == 3
+        assert len(our_data_0._test_pool) == 7
+        assert our_data_0._train_indices == list(our_data_0._train_pool)
+        assert our_data_0._test_indices == list(our_data_0._test_pool)
+        our_big_data.prime_data(order=NNData.Order.RANDOM)
+        assert our_big_data._train_indices != list(our_big_data._train_pool)
+        assert our_big_data._test_indices != list(our_big_data._test_pool)
+    except:
+        print("There are errors that likely come from prime_data")
+        errors = True
+
+    # Make sure get_one_item is returning the correct values, and
+    # that pool_is_empty functions correctly.
+    try:
+        our_data_1.prime_data(order=NNData.Order.SEQUENTIAL)
+        my_x_list = []
+        my_y_list = []
+        while not our_data_1.pool_is_empty():
+            example = our_data_1.get_one_item()
+            print(example)
+            my_x_list.append(list(example[0]))
+            my_y_list.append(list(example[1]))
+        assert len(my_x_list) == 2
+        assert my_x_list != my_y_list
+        my_matched_x_list = [i[0] * 10 for i in my_y_list]
+        assert my_matched_x_list == my_x_list
+        while not our_data_1.pool_is_empty(our_data_1.Set.TEST):
+            example = our_data_1.get_one_item(our_data_1.Set.TEST)
+            my_x_list.append(list(example[0]))
+            my_y_list.append(list(example[1]))
+        assert my_x_list != my_y_list
+        my_matched_x_list = [i[0] * 10 for i in my_y_list]
+        assert my_matched_x_list == my_x_list
+        assert set(i[0] for i in my_x_list) == set(i[0] for i in x)
+        assert set(i[0] for i in my_y_list) == set(i[0] for i in y)
+    except:
+        print("There are errors that may come from prime_data, but could "
+              "be from another method")
+        errors = True
+
+    # Summary
+    if errors:
+        print("You have one or more errors.  Please fix them before "
+              "submitting")
+    else:
+        print("No errors were identified by the unit test.")
+        print("You should still double check that your code meets spec.")
+        print("You should also check that PyCharm does not identify any "
+              "PEP-8 issues.")
+
 
 if __name__ == '__main__':
-    pass
+    unit_test()
